832a40ba82be2dfca4fd32f03063481f075091c7
Teng Mao <50847142+TengCXXI@users.noreply.github.com>
2020-04-03 13:14:12 -0400

    upload password_detection.py and smcd.py

---
+++ password_detection.py
@@ -0,0 +1,26 @@
+#! python3
+# password_detection.py - Allows user to enter a password and check whether it is strong based on the criteria below.
+
+import re
+
+# Create a while loop that asks user to enter a password until a strong one is entered
+while True:
+    print("""Please enter a password that is:
+        *at least 8 characters long
+        *contains both uppercase and lowercase characters
+        *has at least one digit""")
+
+    password = input()
+
+    pass_length = re.compile(r'\w{8,}').search(password) != None    # Checks password is at least 8 characters long
+    pass_upper = re.compile(r'[A-Z]').search(password) != None      # Checks password has upper case
+    pass_lower = re.compile(r'[a-z]').search(password) != None      # Checks password has lower case
+    pass_digit = re.compile(r'[0-9]').search(password) != None      # Checks password has at least one digit
+
+    pass_strong = pass_length and pass_upper and pass_lower and pass_digit # Combines boolean for all checks
+
+    if pass_strong == True:
+        print("Your password is strong!")
+        break
+    else:
+        continue
---
+++ smcd.py
@@ -0,0 +1,117 @@
+#! python3
+# smcd.py - (Selective Move/Copy/Delete) Allows user to move/copy/delete file(s) based on regex filter
+
+import os, re, shutil, send2trash
+import pyinputplus as pyip
+from pathlib import Path
+
+# Create a while loop to check for an absolute folder path input
+while True:
+    print("On what absolute folder path would you like to perform the action?")
+    folder_path = input()
+    if Path(folder_path).exists() == True:
+        break
+    else:
+        print("Please enter a valid absolute folder path.")
+        continue
+
+# Input choice for action to perform
+print("What action would you like to perform on the files in the folder: Move, Copy, or Delete?")
+action = pyip.inputChoice(['Move', 'Copy', 'Delete'])
+
+if action in 'MoveCopy':
+
+    # Create a while loop for the destination folder which will:
+    #     -Check to see if the folder path currently exists
+    #     -If it doesn't exist, ask user whether they want to create the folder path
+    #     -If user wants to create folder path, check to see if the absolute folder path is valid
+    #     -Ask user to provide a valid path if an invalid one was entered
+
+    while True:
+        print("What is the destination folder?")
+        destination_folder = input()
+        if Path(destination_folder).exists() == True:
+            break
+
+        print("This folder path does not currently exist. Would you like to create it - (y)es or (n)o? ")
+        create_path = pyip.inputYesNo()
+        if create_path == "yes" and os.path.isabs(destination_folder) == True:
+            os.makedirs(destination_folder)
+            print(f"{destination_folder} folder created.")
+            break
+
+        else:
+            print("Please enter a valid absolute folder path.")
+            continue
+
+
+    # Create a while loop for the regex input which will:
+    #     -Use regex to select files for action
+    #     -Print out the number of actionable files
+    #     -Ask if the user would like to proceed with the selected files, or re-write the regex statement
+
+
+while True:
+    # Input regex for filter to select files for action
+    print("Please enter the regex to select the files for action:")
+    print('Examples: "\w*\.txt$"(all ".txt" files), "^XyZ\S*"(begins with "XyZ"), \w*XyZ\S*(contains "XyZ"), "|" to add filter')
+    regex_input = input()
+    file_regex = re.compile(r'{0}'.format(regex_input), re.IGNORECASE)
+
+    printout_width = 50
+    print('NUMBER OF ACTIONABLE FILES'.center(printout_width, '='))
+
+    total_files_count = 0
+
+    # Create a loop to count the number of files selected based on regex
+
+    for folder_name, subfolders, files in os.walk(folder_path):
+        folder_files_count = 0
+
+        # Loop to count the files within the folder
+        for file in files:
+            if file_regex.search(file) != None:
+                folder_files_count += 1
+
+        # Increment the total_files by the folder_files count
+        total_files_count += folder_files_count
+        print(folder_name + ': ' + str("{:,}".format(folder_files_count)))
+
+    print('=' * printout_width)
+    print('THE TOTAL NUMBER OF ACTIONABLE FILES: ' + str("{:,}".format(total_files_count)))
+    print('=' * printout_width)
+
+    print("Proceed - (y)es or (n)o?")
+    proceed_with_regex = pyip.inputYesNo()
+    if proceed_with_regex == "yes":
+        break
+
+# TODO: Create a loop to copy the files based on regex
+
+if action == "Copy":
+    for folder_name, subfolders, files in os.walk(folder_path):
+        for file in files:
+            if file_regex.search(file) != None:
+                shutil.copy(str(Path(folder_name)/file), destination_folder)
+
+    print(str("{:,}".format(total_files_count)) + ' file(s) copied.')
+
+# TODO: Create a loop to move the files based on regex
+
+if action == "Move":
+    for folder_name, subfolders, files in os.walk(folder_path):
+        for file in files:
+            if file_regex.search(file) != None:
+                shutil.move(str(Path(folder_name)/file), destination_folder)
+
+    print(str("{:,}".format(total_files_count)) + ' file(s) moved.')
+
+# TODO: Create a loop to delete the files based on regex
+
+if action == "Delete":
+    for folder_name, subfolders, files in os.walk(folder_path):
+        for file in files:
+            if file_regex.search(file) != None:
+                send2trash.send2trash(str(Path(folder_name)/file))
+
+    print(str("{:,}".format(total_files_count)) + ' file(s) deleted.')
